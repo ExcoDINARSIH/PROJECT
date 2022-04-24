1225bbe602a9e218a41b7dac052700ad209ffcc6
Your Name <you@example.com>
2022-04-25 02:03:30 +0700

    Add validation for Food, invalid without name

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -5,9 +5,21 @@ RSpec.describe Food, type: :model do
     food = Food.new(
       name: 'Nasi Kuning',
       description: 'Indonesian rice cooked with coconut milk and turmeric.',
-      price: 15000.0
+      price: 20000.0
     )
 
     expect(food).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    food = Food.new(
+      name: nil,
+      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
+      price: 20000.0
+    )
+
+    food.valid?
+
+    expect(food.errors[:name]).to include("can't be blank")
   end
 end
